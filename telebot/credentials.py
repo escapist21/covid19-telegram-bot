f5125e0f3fe3049c83e6cfb0473b38294b44ef21
Sourav Adhikari <ad.sourav21@live.com>
2020-04-01 16:12:42 +0530

    modified credential

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "904911617:AAENV6txkgo8AbnYFzijHUv91dQgJchscmM"
 bot_user_name = "splitwiseChanchorBot"
-URL = "http://jharkhandcovid19bot.herokuapp.com"
+URL = "https://jharkhandcovid19bot.herokuapp.com/"

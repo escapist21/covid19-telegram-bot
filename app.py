9a3c7840f78677310591287dfb4e52a42e2d7bba
Sourav Adhikari <ad.sourav21@live.com>
2020-04-02 12:34:12 +0530

    added heroku config files

--- app.py
+++ app.py
@@ -10,16 +10,6 @@ FIRST, SECOND, THIRD = range(3)
 ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE = range(9)
 
 
-def send_typing_action(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
-        return func(update, context,  *args, **kwargs)
-
-    return command_func
-
 def start(update, context):
     fname = update.message.from_user.first_name
 
